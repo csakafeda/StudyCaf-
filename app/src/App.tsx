ad256b4b2b063d51fdbc342695e182e17c0e5f47
csakafeda <bako.feodora97@gmail.com>
2023-05-25 00:41:42 +0200

    made basic of project

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,13 +1,25 @@
-import './App.css';
-import HomePage from './component/HomePage';
+import React from 'react';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Home from './component/Home/Home';
+import Navbar from './component/Navbar';
+import Contact from './component/Contact/Contact';
+import Menu from "./component/Menu/Menu";
+import Groups from "./component/Groups/Groups";
 
 function App() {
-  return (
-    <div className="App">
-      <HomePage />
-    </div>
-
-  );  
+    return (
+        <div className="App">
+            <Router>
+                <Navbar/>
+                <Routes>
+                    <Route path="/" element={<Home/>}/>
+                    <Route path="/contact" element={<Contact/>}/>
+                    <Route path="/menu" element={<Menu/>}/>
+                    <Route path="/groups" element={<Groups/>}/>
+                </Routes>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
