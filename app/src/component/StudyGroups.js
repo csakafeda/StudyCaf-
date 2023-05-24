29f54893a0688970c44ae612ffc252289609ba04
csakafeda <bako.feodora97@gmail.com>
2023-05-24 21:39:04 +0200

    add basic menuItems and studyGroups

---
+++ app/src/component/StudyGroups.js
@@ -0,0 +1,20 @@
+export const StudyGroups = [
+    {
+        id: 1,
+        name: 'Math Study Group',
+        description: 'Join us to study and discuss advanced math topics.',
+        members: 10,
+    },
+    {
+        id: 2,
+        name: 'Computer Science Study Group',
+        description: 'Connect with fellow computer science students and explore various topics.',
+        members: 8,
+    },
+    {
+        id: 3,
+        name: 'English Literature Club',
+        description: 'Engage in discussions about classic and contemporary literature.',
+        members: 12,
+    },
+];
--- app/src/component/menuItems.js
+++ app/src/component/menuItems.js
@@ -1,22 +1,35 @@
-const menuItems = [
-  {
-    title: "Cappuccino",
-    description: "Espresso mixed milk..",
-    price: "$3.99",
-    allergeens: "milk",
-  },
-  {
-    title: "Espresso",
-    description: "Strong black coffee.",
-    price: "$2.99",
-    allergeens: "",
-  },
-  {
-    title: "Latte",
-    description: "Espresso with milk.",
-    price: "$4.49",
-    allergeens: "milk",
-  },
+export const MenuItems = [
+    {
+        name: 'Coffee',
+        items: [
+            {name: 'Espresso', price: 2.99},
+            {name: 'Cappuccino', price: 3.99},
+            {name: 'Latte', price: 3.49},
+        ],
+    },
+    {
+        name: 'Sandwiches',
+        items: [
+            {name: 'Ham and Cheese', price: 6.99},
+            {name: 'Turkey Club', price: 7.49},
+            {name: 'Veggie Wrap', price: 5.99},
+        ],
+    },
+    {
+        name: 'Beverages',
+        items: [
+            {name: 'Iced Tea', price: 2.49},
+            {name: 'Lemonade', price: 2.99},
+            {name: 'Soda', price: 1.99},
+        ],
+    },
+    {
+        name: 'Homemade donuts',
+        items: [
+            {name: 'Powder sugar', price: 2.49},
+            {name: 'Vanillia chocholat', price: 4.99},
+            {name: 'Rasberry', price: 1.99},
+        ],
+    },
 ];
 
-export default menuItems;
